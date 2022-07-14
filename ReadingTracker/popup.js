832fb0d28ab739ebf297a111380b3aa0cd3511bc
Sahaj <sahajpatel@posteo.net>
2022-07-13 23:24:49 -0400

    Trying to get cloud working...

--- ReadingTracker/popup.js
+++ ReadingTracker/popup.js
@@ -2,17 +2,17 @@ function addSite() {
 	const name = document.getElementById("name").value;
 	const context = document.getElementById("context").value;
 	const baseurl = document.getElementById("baseurl").value;
-	if (base == null) {
-		console.log("inside if-statement");
-		var myFields = { [context]: name };
-		storage.StorageArea.set(baseurl, JSON.stringify(myFields));
-		console.log(storage.StorageArea(baseurl));
-	}
+	var myFields = { [context]: name };
+	var strFields = JSON.stringify(myFields);
+	browser.storage.sync.set({ baseurl, strFields });
+	console.log(browser.storage.sync.get(baseurl));
+
 }
 
 function getMore() {
 	console.log("Hello World");
-	var temp = storage.StorageArea.get();
+	// var temp = storage.StorageArea.get();
+	var temp = browser.storage.sync.get("test");
 	console.log(temp);
 
 }
@@ -21,4 +21,4 @@ getStorage
 const button = document.getElementById("test");
 const testbtn = document.getElementById("getStorage");
 button.addEventListener("click", addSite);
-testbtn.addEventListener("click", getMore());
+testbtn.addEventListener("click", getMore);
