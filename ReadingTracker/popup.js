c1b3944a7a0397e922a23b89ccca1a2db5f321da
Sahaj <sahajpatel@posteo.net>
2022-08-02 23:22:59 -0400

    Local storage can now be set, retrieved, and cleared.

--- ReadingTracker/popup.js
+++ ReadingTracker/popup.js
@@ -1,24 +1,51 @@
+function successAddSite(result) {
+	var keys = [];
+	for (var k in result) keys.push(k);
+	console.log(keys);
+	for (var key in keys) {
+		console.log("Success at adding the site: " + JSON.stringify(result, key))
+	}
+}
+
+function failureAddSite(error) {
+	console.error("Error adding the site: " + error);
+}
+
+function successCallback(result) {
+	console.log("Retrieved all storage: " + result);
+	var keys = [];
+	for (var k in result) keys.push(k);
+	console.log(keys);
+	for (var key in keys) {
+		console.log(JSON.stringify(result, key))
+	}
+}
+
+function failureCallback(error) {
+	console.error("Failed at getting all storage: " + error);
+}
+
 function addSite() {
 	const name = document.getElementById("name").value;
 	const context = document.getElementById("context").value;
 	const baseurl = document.getElementById("baseurl").value;
-	var myFields = { [context]: name };
-	var strFields = JSON.stringify(myFields);
-	browser.storage.sync.set({ baseurl, strFields });
-	console.log(browser.storage.sync.get(baseurl));
-
+	let newSite = {
+		[name]: { context, baseurl }
+	}
+	var temp = browser.storage.local.set(newSite).then(successAddSite, failureAddSite);
 }
 
-function getMore() {
-	console.log("Hello World");
-	// var temp = storage.StorageArea.get();
-	var temp = browser.storage.sync.get("test");
-	console.log(temp);
-
+function getStorage() {
+	var temp = browser.storage.local.get().then(successCallback, failureCallback);
 }
 
-getStorage
-const button = document.getElementById("test");
-const testbtn = document.getElementById("getStorage");
-button.addEventListener("click", addSite);
-testbtn.addEventListener("click", getMore);
+function clearStorage() {
+	browser.storage.local.clear();
+}
+
+const setStoragebutton = document.getElementById("test");
+const getStorageButton = document.getElementById("getStorage");
+const clearStorageButton = document.getElementById("clearStorage");
+setStoragebutton.addEventListener("click", addSite);
+getStorageButton.addEventListener("click", getStorage);
+clearStorageButton.addEventListener("click", clearStorage);
